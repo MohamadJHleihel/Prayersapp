f23ecfd256fb3aa37313b5f35ba155247af4637a
Mohamad Hleihel <mohammad.helehel@hotmail.com>
2023-11-17 18:15:34 +0100

    Initial Commit

---
+++ Prayersapp/PrayersappApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayersappApp.swift
+//  Prayersapp
+//
+//  Created by Mohamad Hleihel on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct PrayersappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
